8a5599b908e546f7687f88717dc1cca1e937be91
RenanCQueiroz <50963337+RenanCQueiroz@users.noreply.github.com>
2021-10-17 16:20:36 -0400

    Add files via upload

---
+++ Conteiner_aplicacao.py
@@ -0,0 +1,59 @@
+#requisicao CREATE ALUNO 
+
+#requisicao READ ALUNO 
+
+#requisicao UPDATE ALUNO 
+
+#requisicao DELETE ALUNO 
+
+
+#requisicao CREATE CURSO 
+
+#requisicao READ CURSO
+
+#requisicao UPDATE CURSO
+
+#requisicao DELETE CURSO
+
+
+#requisicao CREATE DISCIPLINA
+
+#requisicao READ DISCIPLINA
+
+#requisicao UPDATE DISCIPLINA
+
+#requisicao DELETE DISCIPLINA
+
+
+#requisicao CREATE PROFESSOR
+
+#requisicao READ PROFESSOR
+
+#requisicao UPDATE PROFESSOR
+
+#requisicao DELETE PROFESSOR
+
+
+#requisicao CREATE UNIDADE/BLOCO
+
+#requisicao READ UNIDADE/BLOCO
+
+#requisicao UPDATE UNIDADE/BLOCO
+
+#requisicao DELETE  UNIDADE/BLOCO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
